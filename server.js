e339b4f7fddec3597578d353258571dce1b61b95
Andrew Haight <haight.haight@gmail.com>
2021-10-06 10:06:50 -0700

    test chatwork

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
-<<<<<<< HEAD
-const socket = require('socket.io');
 const express = require('express')
-=======
-const io = require('socket.io')(3001, {
-    cors:{
-        orgin: ['http://localhost:3001'],
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-    },
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = { }
+
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
 })
+
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+  io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
+})
+
+server.listen(3000)
 
 io.on('connection', socket => {
-    console.log('new user')
-    socket.emit('chat-message', 'Hello World')
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
 })
->>>>>>> 2e58ddb02b246212bf31f6fc84292a306c018594
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
