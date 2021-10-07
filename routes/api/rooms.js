e42a73624d44d8ef2c6fe02775da1d7c96858c7a
Andrew Haight <haight.haight@gmail.com>
2021-10-07 10:09:38 -0700

    Co-authored-by: Giselle556 <Giselle556@users.noreply.github.com>

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -1,4 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const router = require("express").Router();
+
+const rooms = {};
+
+router.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+})
 
 router.post("/", (req, res) => {
   if (rooms[req.body.room] != null) {
@@ -16,5 +26,34 @@ router.get("/:room", (req, res) => {
   }
   res.render("room", { roomName: req.params.room });
 });
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (room, name) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (room, message) => {
+    socket.to(room).broadcast.emit("chat-message", {
+      message: message,
+      name: rooms[room].users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    getUserRooms(socket).forEach((room) => {
+      socket
+        .to(room)
+        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id];
+    });
+  });
+});
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name);
+    return names;
+  }, []);
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server); // gives us a server connection to socket.io
 const session = require("express-session");
 const routes = require("./routes");
-const exphbs = require("express-handlebars");
+
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const PORT = process.env.PORT || 3000;
-const hbs = exphbs.create({});
 
 const sess = {
   secret: "Super secret secret",
@@ -22,45 +21,46 @@ const sess = {
   }),
 };
 
-app.set("views", "./views");
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.set("views", "./views"); // setup express server views
+app.set("view engine", "ejs"); // use ejs to parse our views
+app.use(express.static("public")); // javascript goes for client side
+// app.use(express.static("views"));
+app.use(express.urlencoded({ extended: true })); // use url paramaters instead of body for form
 
 app.use(session(sess));
 
-const rooms = {};
+// const rooms = {};
 
 // server.listen(3000);
 
-io.on("connection", (socket) => {
-  socket.on("new-user", (room, name) => {
-    socket.join(room);
-    rooms[room].users[socket.id] = name;
-    socket.to(room).broadcast.emit("user-connected", name);
-  });
-  socket.on("send-chat-message", (room, message) => {
-    socket.to(room).broadcast.emit("chat-message", {
-      message: message,
-      name: rooms[room].users[socket.id],
-    });
-  });
-  socket.on("disconnect", () => {
-    getUserRooms(socket).forEach((room) => {
-      socket
-        .to(room)
-        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
-      delete rooms[room].users[socket.id];
-    });
-  });
-});
+// io.on("connection", (socket) => {
+//   socket.on("new-user", (room, name) => {
+//     socket.join(room);
+//     rooms[room].users[socket.id] = name;
+//     socket.to(room).broadcast.emit("user-connected", name);
+//   });
+//   socket.on("send-chat-message", (room, message) => {
+//     socket.to(room).broadcast.emit("chat-message", {
+//       message: message,
+//       name: rooms[room].users[socket.id],
+//     });
+//   });
+//   socket.on("disconnect", () => {
+//     getUserRooms(socket).forEach((room) => {
+//       socket
+//         .to(room)
+//         .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
+//       delete rooms[room].users[socket.id];
+//     });
+//   });
+// });
 
-function getUserRooms(socket) {
-  return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name);
-    return names;
-  }, []);
-}
+// function getUserRooms(socket) {
+//   return Object.entries(rooms).reduce((names, [name, room]) => {
+//     if (room.users[socket.id] != null) names.push(name);
+//     return names;
+//   }, []);
+// }
 
 app.use(routes);
 
